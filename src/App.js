f42142564cab5bdcf2d3f2ceb3cea64c8215569d
swatinaik2003 <swati622005@gmail.com>
2024-09-21 10:00:03 +0530

    initial origin

--- src/App.js
+++ src/App.js
@@ -10,14 +10,7 @@ import ReportIcon from '@mui/icons-material/Report';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 
-const data = [
-  { name: 'Jan', revenue: 4000, expenses: 2400 },
-  { name: 'Feb', revenue: 3000, expenses: 1398 },
-  { name: 'Mar', revenue: 2000, expenses: 9800 },
-  { name: 'Apr', revenue: 2780, expenses: 3908 },
-  { name: 'May', revenue: 1890, expenses: 4800 },
-  { name: 'Jun', revenue: 2390, expenses: 3800 },
-  { name: 'Jul', revenue: 3490, expenses: 4300 },
+
 ];
 
 // Dummy activities data
